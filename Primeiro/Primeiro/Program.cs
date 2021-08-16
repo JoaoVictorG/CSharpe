da1ec2e9694a46797f3c41e15b8260401b513615
JoaoVictorG <joaovictorgon36@gmail.com>
2021-08-16 18:03:29 -0300

    Média das idades

--- Primeiro/Primeiro/Program.cs
+++ Primeiro/Primeiro/Program.cs
@@ -6,7 +6,20 @@ namespace Primeiro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Qual a idade da primera pessoa?");
+            int idade1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual a idade da segunda pessoa?");
+            int idade2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual a idade da terceira pessoa?");
+            int idade3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual a idade da quarta pessoa?");
+            int idade4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual a idade da quinta pessoa?");
+            int idade5 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = (idade1 + idade2 + idade3 + idade4 + idade5) / 5;
+            Console.WriteLine($"A idade média é {resultado}");
+            
         }
     }
 }
