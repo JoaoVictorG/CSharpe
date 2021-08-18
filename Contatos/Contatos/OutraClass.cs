211703d01dabfa8e8876d474dfdd8a4b3ed7bbd7
JoaoVictorG <joaovictorgon36@gmail.com>
2021-08-18 16:59:55 -0300

    Nova Classe

---
+++ Contatos/Contatos/OutraClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Contatos
+{
+    class OutraClass
+    {
+        public static void Main()
+        {
+            MessageBox.Show("Pow!");
+            for (int i = 0; i < 8; i = i + 2)
+            {
+                MessageBox.Show($"Vou aparecer 4 vezes {i}");
+            }
+        }
+    }
+}
