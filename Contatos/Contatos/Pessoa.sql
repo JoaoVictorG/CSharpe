08bfe8e47a2d54642bedbb77f7289543d229a2a6
JoaoVictorG <joaovictorgon36@gmail.com>
2021-08-18 11:41:10 -0300

    Criação do Banco de Dados

---
+++ Contatos/Contatos/Pessoa.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Pessoa
+(
+	[ContactID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(50) NULL, 
+    [Empresa] NVARCHAR(50) NULL, 
+    [Telefone] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Cliente] BIT NULL, 
+    [Ultima Ligação] DATETIME NULL
+)
