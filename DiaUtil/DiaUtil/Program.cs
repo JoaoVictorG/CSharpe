1949fa970b88706f808482fdff04021d901cad31
JoaoVictorG <joaovictorgon36@gmail.com>
2021-08-31 17:16:09 -0300

    Estrutura dia Útil

---
+++ DiaUtil/DiaUtil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiaUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
