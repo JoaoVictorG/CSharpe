260193ae6b37ec1a86ea10a19623bbefb1b4c9fe
JoaoVictorG <joaovictorgon36@gmail.com>
2021-08-31 10:15:48 -0300

    Texto com quebra de linha

--- Welcome1/Welcome1/Welcome1.cs
+++ Welcome1/Welcome1/Welcome1.cs
@@ -6,7 +6,9 @@ namespace Welcome1
     {
         static void Main()
         {
+            Console.Write("Olá");
             Console.WriteLine("Hello World!");
+            Console.Write("Olá");
         }
     }
 }
