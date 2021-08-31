0e139bf69299fe568b2aaccd76192ad3a9ad413c
JoaoVictorG <joaovictorgon36@gmail.com>
2021-08-31 17:26:01 -0300

    Finalização

--- DiaUtil/DiaUtil/Program.cs
+++ DiaUtil/DiaUtil/Program.cs
@@ -6,7 +6,21 @@ namespace DiaUtil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o dia:");
+            int dia = int.Parse(Console.ReadLine());
+            if(dia > 1 && dia <= 5)
+            {
+                Console.WriteLine("Dia útil");
+            }
+            if(dia == 6 || dia == 7)
+            {
+                Console.WriteLine("Fim de semana");
+            }
+            else
+            {
+                Console.WriteLine("Inválido");
+            }
+
         }
     }
 }
