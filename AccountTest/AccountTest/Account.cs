27c56f2993203b9cf58fc0bab49a56be3f7174ca
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-08 11:43:00 -0300

    New class

---
+++ AccountTest/AccountTest/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    class Account
+    {
+        private string name;
+        public void SetName(string accountName)
+        {
+            name = accountName;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
