2787a7ae4a2b7094e75ebcca840c733782446957
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-08 16:08:45 -0300

    Teste

---
+++ AccountTest/AccountTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AccountTest
+{
+    class AccountTeste
+    {
+        static void Main()
+        {
+            Account myAccount = new Account();
+            Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+
+            Console.Write("Enter the name: ");
+            string theName = Console.ReadLine();
+            myAccount.SetName(theName);
+
+            Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+        }
+    }
+}
