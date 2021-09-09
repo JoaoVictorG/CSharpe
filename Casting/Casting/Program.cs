4b814280655f43052a5b8290420798cbd46c221d
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-09 11:19:49 -0300

    Sem perda de informação

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b);
+        }
+    }
+}
