8c6d8a15518e0dfd96dd4195036601cdef1742ec
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-09 11:58:18 -0300

    Divisão de Cores

---
+++ Split/Split/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome de três cores");
+            string cor = Console.ReadLine();
+            string[] vet = cor.Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
