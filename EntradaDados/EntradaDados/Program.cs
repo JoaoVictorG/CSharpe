001db88b6c007f7da7cf8bac9b086f5f2c8d9475
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-09 15:35:26 -0300

    Atividade Finalizada

---
+++ EntradaDados/EntradaDados/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace EntradaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double produto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha)");
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2]);
+            Console.WriteLine(ultNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString(CultureInfo.InvariantCulture));
+
+        }
+    }
+}
