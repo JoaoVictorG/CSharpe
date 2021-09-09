6751a2e1fc75ddec03ea8a1e78637a521786f096
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-09 10:05:12 -0300

    Duas Casas Decimais

---
+++ CasasDecimais/CasasDecimais/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+namespace CasasDecimais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double valor = 1243.987;
+            Console.WriteLine(valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
