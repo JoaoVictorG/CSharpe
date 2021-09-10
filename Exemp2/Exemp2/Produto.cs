3e12e0bad1adb06df4df60507b22bfe679b7cc07
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 16:37:18 -0300

    Concatenação

--- Exemp2/Exemp2/Produto.cs
+++ Exemp2/Exemp2/Produto.cs
@@ -15,7 +15,13 @@ namespace Exemp2
         }
         public override string ToString()
         {
-            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades.";
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantidade 
+                + " unidades., Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
