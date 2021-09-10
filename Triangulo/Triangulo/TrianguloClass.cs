77cec07b2a26c7313a5054d03e7640fb0c5b7aed
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 11:59:28 -0300

    Function

--- Triangulo/Triangulo/TrianguloClass.cs
+++ Triangulo/Triangulo/TrianguloClass.cs
@@ -9,7 +9,11 @@ namespace Triangulo
         public double A;
         public double B;
         public double C;
-        public double Area;
-        public double P;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
