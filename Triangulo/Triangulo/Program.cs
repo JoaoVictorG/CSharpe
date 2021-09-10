2d3622b3a7378f3c8f376177e8130a9bb9d4f129
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 08:44:46 -0300

    Estrutura Base

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
