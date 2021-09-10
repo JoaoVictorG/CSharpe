760976ef94e8805e028615dc5f472931f7112b3e
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 14:59:43 -0300

    Frase

---
+++ Exemp2/Exemp2/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemp2
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+    }
+}
