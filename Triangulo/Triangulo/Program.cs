f8db9e291d8c03641f6dc43a8907f1faa8ff0b85
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 08:57:55 -0300

    Área dos Triângulos

--- Triangulo/Triangulo/Program.cs
+++ Triangulo/Triangulo/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Triangulo
 {
     class Program
@@ -11,10 +11,18 @@ namespace Triangulo
             double bX = double.Parse(Console.ReadLine());
             double cX = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Entre com as medidas do triângulo y:");
+            double aY = double.Parse(Console.ReadLine());
+            double bY = double.Parse(Console.ReadLine());
+            double cY = double.Parse(Console.ReadLine());
+
             double pX = (aX + bX + cX) / 2;
             double areaX = Math.Sqrt(pX * (pX - aX) * (pX - bX) * (pX - cX));
+            Console.WriteLine(areaX.ToString("F4", CultureInfo.InvariantCulture));
 
-            Console.WriteLine(areaX.ToString("F4"));
+            double pY = (aY + bY + cY) / 2;
+            double areaY = Math.Sqrt(pY * (pY - aY) * (pY - bY) * (pY - cY));
+            Console.WriteLine(areaY.ToString("F4",CultureInfo.InvariantCulture));
         }
     }
 }
