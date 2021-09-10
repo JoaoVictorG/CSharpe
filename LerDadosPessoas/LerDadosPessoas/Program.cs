2f03f77a5c1ab8fd4045d04eb0e5387eda68af9b
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 11:02:45 -0300

    Estrutura Base

---
+++ LerDadosPessoas/LerDadosPessoas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LerDadosPessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
