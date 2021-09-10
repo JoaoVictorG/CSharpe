be927ea8aea4ff0617f9a31f8e35e49327cee6ed
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 17:45:22 -0300

    Estrutura Base

---
+++ PagamentoImposto/PagamentoImposto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PagamentoImposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
