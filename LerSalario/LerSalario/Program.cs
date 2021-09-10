c79dfd6849fb3f596297ba62be14d6e81b59858a
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-10 11:18:45 -0300

    Estrutura Base

---
+++ LerSalario/LerSalario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LerSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
