3516a0e009b1a63f638e2cee171e02ee5df43e6f
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-13 10:57:10 -0300

    Estrutura Base

---
+++ Circunferencia/Circunferencia/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Circunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o Valor do Raio: ");
+
+        }
+    }
+}
