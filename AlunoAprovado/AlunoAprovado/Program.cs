13a2ddc26a83077bdbef63b8df82ef5c5509e0f6
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-13 09:42:34 -0300

    Concluido

--- AlunoAprovado/AlunoAprovado/Program.cs
+++ AlunoAprovado/AlunoAprovado/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace AlunoAprovado
 {
@@ -6,7 +7,26 @@ namespace AlunoAprovado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var al = new Aluno();
+            Console.WriteLine("Qual o nome do Aluno?");
+            al.Nome = Console.ReadLine();
+            Console.WriteLine("Qual a nota do primeiro trimestre?");
+            al.Nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a nota do segundo trimestre?");
+            al.Nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a nota do terceiro trimestre?");
+            al.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota Final: " + al.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            if (al.Aprovacao())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram: " + al.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " pontos");
+            }
         }
     }
 }
