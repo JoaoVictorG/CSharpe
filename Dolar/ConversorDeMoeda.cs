62ac8eba5b4fd3cdc2a778d6fc77c5e000eb06cb
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-16 12:00:17 -0300

    Att

---
+++ Dolar/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dolar
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double Calculo(double p,double q)
+        {
+            return (p * q) * Iof;  
+        }
+
+    }
+}
