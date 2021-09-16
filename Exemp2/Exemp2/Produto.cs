ead05a0e2997a801df6680682fec7686302f039d
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-16 17:16:06 -0300

    Encapsulamento

--- Exemp2/Exemp2/Produto.cs
+++ Exemp2/Exemp2/Produto.cs
@@ -5,41 +5,54 @@ namespace Exemp2
 {
     class Produto
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto(string nome, double preco, int quantidade)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
-        public Produto(string nome, double preco)
+        public string GetNome()
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = 5;
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if(nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+        public double GetPreco()
+        {
+            return _preco;
+        }
+        public int GetQuantidade()
+        {
+            return _quantidade;
         }
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
         public void AdicionarProduto(int quantidade)
         {
-            Quantidade = Quantidade + quantidade;
+            _quantidade = _quantidade + quantidade;
         }
         public void RemoverProduto(int quantidade)
         {
-            Quantidade = Quantidade - quantidade;
+            _quantidade = _quantidade - quantidade;
         }
         public override string ToString()
         {
-            return Nome 
+            return _nome 
                 + ", $ " 
-                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + _preco.ToString("F2", CultureInfo.InvariantCulture) 
                 + ", " 
-                + Quantidade 
+                + _quantidade 
                 + " unidades., Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
