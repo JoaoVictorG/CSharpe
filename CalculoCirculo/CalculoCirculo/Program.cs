96f4804a57d55fc8d387b0f12065de1243b57038
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-16 10:11:07 -0300

    Primeira Fase

---
+++ CalculoCirculo/CalculoCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculo();
+            Console.WriteLine("Digite o valor do Raio");
+            calc.Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Circuferência: " + calc.Circunferencia().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + calc.Volume().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
