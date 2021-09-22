e8a34a39bdc5dbe6e0a5c63f47e7bf8464d24bd8
JoaoVictorG <joaovictorgon36@gmail.com>
2021-09-22 15:24:38 -0300

    Conclusão

--- Nullable/Nullable/Program.cs
+++ Nullable/Nullable/Program.cs
@@ -6,7 +6,37 @@ namespace Nullable
     {
         static void Main(string[] args)
         {
-            double x;
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+
+            double? a = x ?? 5;
+            double? b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
