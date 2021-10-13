e4b49386ac6dbb591d01365bb442951bf90585c8
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-13 11:12:55 -0300

    Mostrando o valor do elemento 0

--- ArrayCSharpeLivro/ArrayCSharpeLivro/Program.cs
+++ ArrayCSharpeLivro/ArrayCSharpeLivro/Program.cs
@@ -6,11 +6,12 @@ namespace ArrayCSharpeLivro
     {
         static void Main(string[] args)
         {
-            int[] primeiraArray = new int[100];
+            //int[] primeiraArray = new int[100];
             int[] segundoArray = new int[] { 1, 3, 5, 7, 9 };
-            int[] terceiroArray = { 1, 3, 5, 7, 9 };
+            /*int[] terceiroArray = { 1, 3, 5, 7, 9 };
             int[,] arrayMultiDimensional = new int[2, 3];
-            int[,] arrayMultiDimensional2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] arrayMultiDimensional2 = { { 1, 2, 3 }, { 4, 5, 6 } };*/
+            Console.WriteLine("Valor do array na posição 0 -> {0} ", segundoArray[0]);
         }
     }
 }
