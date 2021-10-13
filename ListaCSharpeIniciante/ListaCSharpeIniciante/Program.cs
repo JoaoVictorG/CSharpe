1b6fd98fffa6bde9c5a2500d99b22ceae57be2df
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-13 11:25:32 -0300

    Visualização dos elementos da Lista

--- ListaCSharpeIniciante/ListaCSharpeIniciante/Program.cs
+++ ListaCSharpeIniciante/ListaCSharpeIniciante/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ListaCSharpeIniciante
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> nomesFuncionarios = new List<string>();
+            nomesFuncionarios.Add("Maria");
+            nomesFuncionarios.Add("João");
+            nomesFuncionarios.Add("André");
+            nomesFuncionarios.Add("Flávia");
+
+            Console.WriteLine();
+            foreach(string pessoa in nomesFuncionarios)
+            {
+                Console.WriteLine(pessoa);
+            }
         }
     }
 }
