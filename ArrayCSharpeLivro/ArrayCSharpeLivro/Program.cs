efe309f09dc7611f080ebf717a0e55410abac7fc
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-13 10:31:04 -0300

    Criando arrays

---
+++ ArrayCSharpeLivro/ArrayCSharpeLivro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayCSharpeLivro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] primeiraArray = new int[100];
+            int[] segundoArray = new int[] { 1, 3, 5, 7, 9 };
+            int[] terceiroArray = { 1, 3, 5, 7, 9 };
+            int[,] arrayMultiDimensional = new int[2, 3];
+            int[,] arrayMultiDimensional2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+        }
+    }
+}
