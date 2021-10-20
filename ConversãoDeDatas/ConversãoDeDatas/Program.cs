ab2f3c3f62d3771cfbb789df31da0708060f2ef0
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-20 16:23:57 -0300

    Conversão

--- ConversãoDeDatas/ConversãoDeDatas/Program.cs
+++ ConversãoDeDatas/ConversãoDeDatas/Program.cs
@@ -11,11 +11,21 @@ namespace ConversãoDeDatas
             DateTime dataTextoConvertida;
             if(DateTime.TryParse(dataTexto, out dataTextoConvertida))
             {
-                Console.WriteLine("Data com conexão Aceita");
+                Console.WriteLine("Data com conversão Aceita");
             }
             else
             {
-                Console.WriteLine("Erro na conexão");
+                Console.WriteLine("Erro na conversão");
+            }
+            string dataTextoErrada = "20/mês 10/2021";
+            DateTime dataTextoErradaConvertida;
+            if(DateTime.TryParse(dataTextoErrada, out dataTextoErradaConvertida))
+            {
+                Console.WriteLine("Data com conversão Aceita");
+            }
+            else
+            {
+                Console.WriteLine("Erro na conversão");
             }
         }
     }
