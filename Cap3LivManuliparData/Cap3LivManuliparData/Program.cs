0c48d2680ab1d6a4714e7d51c3b0c073f2954c42
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-20 11:20:38 -0300

    Manipulação de datas

---
+++ Cap3LivManuliparData/Cap3LivManuliparData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace Cap3LivManuliparData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dtPedido = DateTime.Today;
+            DateTime dtVencto = dtPedido.AddDays(35);
+
+            DateTime dtPago = dtVencto.AddMonths(2);
+            Console.WriteLine($"Pedidio feito e: {dtPedido: dd/MMM/yyyy} vence em {dtVencto: dd/MMM/yyyy}");
+
+            Console.WriteLine($"Formatação Completa: {dtVencto.ToLongDateString()}");
+            Console.WriteLine($"Formatação Completa: {dtVencto.ToLongDateString()}");
+
+            Console.WriteLine($"Dia da semana: {dtVencto.DayOfWeek}");
+            Console.WriteLine($"Dia da semana em português {dtVencto.ToString("dddd", new CultureInfo("pt-BR"))}");
+        }
+    }
+}
