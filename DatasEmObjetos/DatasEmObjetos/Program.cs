05c3dae87c5f8fd186ae6f6ec98be43fdbb3ae10
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-21 17:21:03 -0300

    Informações

--- DatasEmObjetos/DatasEmObjetos/Program.cs
+++ DatasEmObjetos/DatasEmObjetos/Program.cs
@@ -6,7 +6,16 @@ namespace DatasEmObjetos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pedido = new Pedido
+            {
+                PedidoId = 1,
+                DtPedido = DateTime.Today,
+                DtPagto = DateTime.Today.AddDays(45),
+                Valor = 1500
+            };
+            Console.WriteLine($"Pedido : {pedido.PedidoId} - " + $"Data: {pedido.DtPedido:dd/MMM/yyyy} - " + $"Vencimento: {pedido.DtVencimento(): dd/MMM/yyyy}");
+
+            
         }
     }
 }
