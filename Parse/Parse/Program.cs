9962bdd7a7096ef0a87abaef705472d0cc218e59
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-22 13:47:15 -0300

    Convertendo string para inteiro

---
+++ Parse/Parse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("----- . Parse");
+                Console.WriteLine($"{Int16.Parse("150")}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
