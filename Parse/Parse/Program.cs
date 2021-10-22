e360d10c459e2177315031aa5fd34e296a75a5d5
JoaoVictorG <joaovictorgon36@gmail.com>
2021-10-22 15:30:14 -0300

    Adição de hora

--- Parse/Parse/Program.cs
+++ Parse/Parse/Program.cs
@@ -6,7 +6,7 @@ namespace Parse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Data: {DateTime.Parse("22/10/2021")}");
+            Console.WriteLine($"Data: {DateTime.Parse("22/10/2021 15:59:30")}");
         }
     }
 }
