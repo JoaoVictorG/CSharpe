a68fefa5872b6b368e214d42f67a7f9a416ab3f9
JoaoVictorG <joaovictorgon36@gmail.com>
2022-05-24 10:32:57 -0300

    funcionalidades do jogo

--- PedraPapelTesoura/PedraPapelTesoura/Program.cs
+++ PedraPapelTesoura/PedraPapelTesoura/Program.cs
@@ -1,7 +1,62 @@
 ﻿using System;
 
-class main{
-    Random random = new Random();
+namespace PedraPapelTesoura
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Escolha sua jogada:");
+            Console.WriteLine("0 - Pedra");
+            Console.WriteLine("1 - Papel");
+            Console.WriteLine("2 - Tesoura");
+            
+            //Console.WriteLine("Você"jogadaHumano);
+            int i = 1;
+            while (i != 2)
+            {
+                int jogadaHumano = int.Parse(Console.ReadLine());
+                Random rnd = new Random();
 
-    Console.WriteLine($"{random.Next(),15:N0}");
+                int jogadaComputador = rnd.Next(0, 3);
+                
+                Console.WriteLine("Você escolheu: ");
+                Console.WriteLine(jogadaHumano);
+                Console.WriteLine("Seu adiversário escolheu: ");
+                Console.WriteLine(jogadaComputador);
+
+                //Console.WriteLine(numeroAleatorio);
+                if (jogadaHumano - jogadaComputador == 0)
+                {
+                    Console.WriteLine("Você empatou");
+                }
+                else if (jogadaHumano - jogadaComputador == (-2) || jogadaHumano - jogadaComputador == 1)
+                {
+                    Console.WriteLine("Você ganhou");
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Você perdeu");
+                }
+
+
+            }    
+            //string textoHumano;
+            //if (jogadaHumano == 0)
+            //{
+            //    textoHumano = "Pedra";
+            //}
+            //else if(jogadaHumano == 1)
+            //{
+            //    textoHumano = "Papel";
+            //}
+            //else if(jogadaHumano == 2)
+            //{
+            //    textoHumano = "Tesoura";
+            //}
+
+            //Console.WriteLine($"Você escolheu: {textoHumano}");
+        }
+    }
 }
