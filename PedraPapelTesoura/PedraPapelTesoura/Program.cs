b8c2900693b731184ea00d22c184657ecd634613
JoaoVictorG <joaovictorgon36@gmail.com>
2022-05-24 09:02:14 -0300

    estrutura basica

---
+++ PedraPapelTesoura/PedraPapelTesoura/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+class main{
+    Random random = new Random();
+
+    Console.WriteLine($"{random.Next(),15:N0}");
+}
---
+++ PedraPapelTesoura/PedraPapelTesoura/escolherOpcao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedraPapelTesoura
+{
+    internal class escolherOpcao
+    {
+        Random random = new Random();
+
+        Console.WriteLine($"{random.Next(),15:N0}");
+
+        return value;
+    }
+}
